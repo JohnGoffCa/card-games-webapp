e912c1521731b082dc32804f8132b7d2dbf0a79e
elijah <eli.heineman@gmail.com>
2017-08-16 21:17:12 +0000

    added game start query

--- queries.js
+++ queries.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
+
 let knex = require('knex')({
-  client: 'postgresql'
+  client: 'postgresql',
 	connection: {
 	  host     : process.env.DB_HOST,
 	  user     : process.env.DB_USER,
@@ -10,15 +12,44 @@ let knex = require('knex')({
 	}
 });
 
-knex('users').where({
-	username: 
-})
-.select('id')
-
-
 function findUser(username) {
-	knex('users').where({
+	return knex('users').where({
     username: username
 	})
 	.select('id')
+	.then((results)=>{
+		if(results.length > 0) {
+			results.forEach((result)=>{
+	      console.log(result.id)
+			})
+			return results;
+		} else {
+			console.log("Not found");
+		}
+
+	}).catch((err) => {
+		console.log(err);
+	})
+	.finally(function () {
+    return knex.destroy();
+  })
 }
+findUser('Bob')
+//ask about using promises to get data
+// function insert(obj) {
+// 	return knex('').insert(...);
+// }
+
+// findUser('Bob').then((results) => {
+// 	var obj = {..}
+// 	return insert(obj);
+// }).then(() => {
+
+function addPlayersToGame(player1_id, player2_id, url, complete){
+	return knex('gamesessions').insert({player1_id: player1_id, player2_id: player2_id, url: url, complete: false})
+	       .then((result) => {
+	       	console.log('success')
+	       })
+}
+
+addPlayersToGame(1, 2, 'xyz')
