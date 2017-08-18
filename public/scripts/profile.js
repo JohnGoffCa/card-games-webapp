6df12465604a763dad7d9672d5c1d7df5806ab1c
elijah <eli.heineman@gmail.com>
2017-08-18 19:27:20 +0000

    added archive render

---
+++ public/scripts/profile.js
@@ -0,0 +1,33 @@
+function createArchiveElement(archive){
+	return `
+	        <tr>
+	        <th>Game</th>
+	        <th>Player 1</th>
+	        <th>Player 2</th>
+	        <th>Player 1 Score</th>
+	        <th>Player 2 Score</th>
+	      </tr>
+	      <tr>
+	        <td>${archive.gameinfo_id}</td>
+	        <td>${archive.player1_id}</td>
+	        <td>${archive.player2_id}</td>
+	        <td>${archive.p1_finalscore}</td>
+	        <td>${archive.p2_finalscore}</td>
+	      </tr>`
+}
+
+const renderArchive = (data) => {
+	const games = data.map(createArchiveElement)
+	const html = games.reverse().join('')
+	$('#archives').html(html)
+}
+
+const loadArchive = () => {
+	$.get(`/profile/${ App.userId }/json`, function(data){
+		renderArchive(data)
+	})
+}
+
+$(function(){
+  loadArchive()
+})
