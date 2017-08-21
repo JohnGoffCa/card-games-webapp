d48dd50358f9d152a716b3cf973a1a97558f7115
John Goff <gygn.johngoff@gmail.com>
2017-08-21 10:23:25 -0400

    made a bit prettier

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,26 +1,5 @@
-//$(() => {
-//  $.ajax({
-//    method: 'GET',
-//    url: '/api/users'
-//  }).done((users) => {
-//    for(user of users) {
-//      $('<div>').text(user.name).appendTo($('body'));
-//    }
-//  });;
-//});
-
-$(document).ready(() => {
-  $('.match-btn').on('click', () => {
-    //$.ajax({
-    //  method: 'POST',
-    //  url: '/api/goofspiel/1', //TODO make this something other than 1 hardcoded
-    //  data: JSON.stringify({
-    //    player1ID: "p1",       //TODO fix
-    //    player2ID: "p2",       //TODO fix
-    //  }),
-    //}).done(() => {
-    //  window.location = '/game/goofspiel/1';
-    //});
+$(document).ready(function () {
+  $('.match-btn').on('click', function () {
     location.href = '/newgame';
   });
 });
